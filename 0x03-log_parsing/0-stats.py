1a77509976ea0e81e382fc2b8536b8598d65acff
Reigns <lreigns12@gmail.com>
2023-10-20 15:14:00 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''a script that reads stdin line by line and computes metrics'''
 
+
 import sys
 
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
@@ -10,27 +11,27 @@ counter = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split()
-        if len(line_list) >= 10:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in cache:
+            if code in cache.keys():
                 cache[code] += 1
             total_size += size
             counter += 1
 
         if counter == 10:
+            counter = 0
             print('File size: {}'.format(total_size))
             for key, value in sorted(cache.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
-            counter = 0
 
-except KeyboardInterrupt:
-    # Handle a keyboard interrupt (CTRL + C)
+except Exception as err:
+    pass
+
+finally:
     print('File size: {}'.format(total_size))
     for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
-except Exception as err:
-    pass
