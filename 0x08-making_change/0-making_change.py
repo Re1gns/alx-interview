c143e0dbd77b57749fd907969ba5fa62bca99a34
Re1gns <lreigns12@gmail.com>
2023-11-21 17:07:19 +0100

    Making a change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,16 +14,17 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
+    MAX_VALUE = float('inf')
+    table = [MAX_VALUE for i in range(total + 1)]
     table[0] = 0
     m = len(coins)
     for i in range(1, total + 1):
         for j in range(m):
             if coins[j] <= i:
                 subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
+                if subres != MAX_VALUE and subres + 1 < table[i]:
                     table[i] = subres + 1
 
-    if table[total] == sys.maxsize:
+    if table[total] == MAX_VALUE:
         return -1
     return table[total]
